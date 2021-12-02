1ddecb2a327b63f14a77646fa88d33785cd0aa8c
repnop <repnop@repnop.dev>
2021-12-02 08:57:39 -0500

    Day 2

---
+++ src/day02.rs
@@ -0,0 +1,50 @@
+pub fn run(input: &str) {
+    let input = parse(input);
+    println!("day 02 - part 1: {}", part_1(&input));
+    println!("day 02 - part 2: {}", part_2(&input));
+}
+
+#[derive(Clone, Copy)]
+enum Command {
+    Forward(u32),
+    Down(u32),
+    Up(u32),
+}
+
+fn parse(input: &str) -> Vec<Command> {
+    input
+        .lines()
+        .map(|s| {
+            let (command, value) = s.split_once(' ').unwrap();
+            let value = value.parse().unwrap();
+            match command {
+                "forward" => Command::Forward(value),
+                "down" => Command::Down(value),
+                "up" => Command::Up(value),
+                _ => unreachable!(),
+            }
+        })
+        .collect()
+}
+
+fn part_1(input: &[Command]) -> u32 {
+    let (horizontal, depth) =
+        input.iter().copied().fold((0, 0), |(horizontal, depth), command| match command {
+            Command::Forward(value) => (horizontal + value, depth),
+            Command::Down(value) => (horizontal, depth + value),
+            Command::Up(value) => (horizontal, depth.wrapping_sub(value)),
+        });
+
+    horizontal * depth
+}
+
+fn part_2(input: &[Command]) -> u32 {
+    let (_, horizontal, depth) =
+        input.iter().copied().fold((0, 0, 0), |(aim, horizontal, depth), command| match command {
+            Command::Forward(value) => (aim, horizontal + value, depth + aim * value),
+            Command::Down(value) => (aim + value, horizontal, depth),
+            Command::Up(value) => (aim.wrapping_sub(value), horizontal, depth),
+        });
+
+    horizontal * depth
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod day01;
+mod day02;
 
-static DAYS: &'static [fn(&str)] = &[
-    day01::run
-];
+static DAYS: &'static [fn(&str)] = &[day01::run, day02::run];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     for (i, day) in DAYS.iter().enumerate() {
